0f2cd9668ab40d68b92e4da2658e043d18c91944
Clemens Krack <ckrack@gmail.com>
2014-12-11 17:46:01 +0100

    - fixed bugs with persistence of data. - moved the data events into one place instead of the setup functions for the dialog elements. - removed CKSource from authors, since this was a complete rewrite instead of a fork of image2 now. - added correct language strings.

--- plugin.js
+++ plugin.js
@@ -1,6 +1,6 @@
 ﻿/**
- * @license Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
- * @author CKSource - Frederico Knabben. / Clemens Krack <info@clemenskrack.com>
+ * @license Copyright (c) 2003-2014, Clemens Krack. All rights reserved.
+ * @author Clemens Krack <info@clemenskrack.com>
  * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
@@ -8,77 +8,90 @@
 
 ( function() {
 
-
 	CKEDITOR.plugins.add( 'imageresponsive', {
 		lang: 'en,de',
 		requires: 'widget,dialog,image2',
-
-		beforeInit: function( editor ) {
-			editor.widgets.on('instanceCreated', function(e){
-				// figure out if this is the image dialog.
-				if(e.data.name != 'image')
-					return;
-
-				// add allowedContent
-				e.data.allowedContent.img.attributes = '!src,alt,width,height,srcset,sizes';
-			});
-
-
-			CKEDITOR.on( 'dialogDefinition', function( ev ) {
-				if ((ev.editor != editor) || (ev.data.name != 'image2'))
-					return;
-				// Take the dialog name and its definition from the event data.
-				// Check if the definition is from the dialog we're
-				// interested on
-				// Get a reference to the "Info" tab.
-				var infoTab = ev.data.definition.getContents( 'info' );
-
-				// Add a new text field to the "info" tab page.
-				infoTab.add(
-					{
-						id: 'srcset',
-						type: 'text',
-						label: 'Quellenverzeichnis' + this.lang.srcset,
-						setup: function( widget ) {
-							this.setValue(widget.data.srcset);
-							// register handler for data
-							widget.on('data', function(e){
-								// keep srcset & sizes attributes only when set.
-								if(e.data.srcset) {
-									e.sender.parts.image.setAttribute('srcset', e.data.srcset);
-								} else {
-									e.sender.parts.image.removeAttribute('srcset');
-								}
-							});
-						},
-                        commit: function ( widget ) {
-                            widget.setData('srcset', this.getValue());
-                        }
-					}
-				);
-				infoTab.add(
-					{
-						id: 'sizes',
-						type: 'text',
-						label: 'Sizes' + this.lang.sizes,
-						setup: function( widget ) {
-							this.setValue(widget.data.sizes);
-							// register handler for data
-							widget.on('data', function(e){
-								// keep sizes attributes only when set.
-								if(e.data.sizes) {
-									e.sender.parts.image.setAttribute('sizes', e.data.sizes);
-								} else {
-									e.sender.parts.image.removeAttribute('sizes');
-								}
-							});
-						},
-                        commit: function ( widget ) {
-                            widget.setData('sizes', this.getValue());
-                        }
-					}
-				);
-			});
+		allowedContent:  {
+			img: {
+				attributes: 'srcset,sizes'
+			}
 		},
+
+		init: function( editor ) {
+
+			// bind to widget#instanceCreated so we can see when the image widget is about to be initiated
+			editor.widgets.on('instanceCreated', function(e) {
+
+				var widget = e.data;
+
+				// figure out if this is the image dialog.
+				if(widget.name != 'image')
+					return;
+
+				// register handler for data
+				widget.on('data', function(e) {
+					widget = e.data;
+					// keep srcset & sizes attributes only when set.
+					if(widget.srcset)
+						e.sender.parts.image.setAttribute('srcset', widget.srcset);
+					else
+						e.sender.parts.image.removeAttribute('srcset');
+
+					if(widget.sizes)
+						e.sender.parts.image.setAttribute('sizes', widget.sizes);
+					else
+						e.sender.parts.image.removeAttribute('sizes');
+				});
+
+				// set data from existing variables.
+				var image = widget.element;
+
+				// since the img-tag can be wrapped with a caption, make sure we use the right element.
+				if(image.getName() != 'img')
+					image = image.findOne('img');
+
+				var data = {
+					srcset: image.getAttribute( 'srcset' ) || '',
+					sizes: image.getAttribute( 'sizes' ) || '',
+				};
+				widget.setData(data);
+			});
+
+			CKEDITOR.on( 'dialogDefinition', function(e) {
+				// make sure this is the right editor (there can be more on one page) and the right dialog.
+				if ((e.editor != editor) || (e.data.name != 'image2'))
+					return;
+
+				// Get a reference to the "Link Info" tab.
+				var infoTab = e.data.definition.getContents( 'info' );
+
+				// Add text fields for srcset and sizes.
+				infoTab.add({
+					id: 'srcset',
+					type: 'text',
+					requiredContent: 'img[srcset]',
+					label: e.editor.lang.imageresponsive.srcset,
+					setup: function(widget) {
+						this.setValue(widget.data.srcset);
+					},
+                    commit: function (widget) {
+                        widget.setData('srcset', this.getValue());
+                    }
+					});
+
+				infoTab.add({
+					id: 'sizes',
+					type: 'text',
+					requiredContent: 'img[sizes]',
+					label: e.editor.lang.imageresponsive.sizes,
+					setup: function(widget) {
+						this.setValue(widget.data.sizes);
+					},
+                    commit: function (widget) {
+                        widget.setData('sizes', this.getValue());
+                    }
+				});
+			});
+		}
 	} );
 } )();
