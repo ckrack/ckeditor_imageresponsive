a343ad35fd2cdb54583f0acd3759545db47ad6d7
Clemens Krack <ckrack@gmail.com>
2014-12-11 17:57:32 +0100

    removed other language strings.

--- lang/de.js
+++ lang/de.js
@@ -3,20 +3,6 @@ Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.md or http://ckeditor.com/license
 */
 CKEDITOR.plugins.setLang( 'imageresponsive', 'de', {
-	alt: 'Alternativer Text',
-	btnUpload: 'Zum Server senden',
-	captioned: 'Bild mit Überschrift',
-	captionPlaceholder: 'Überschrift',
-	infoTab: 'Bild-Info',
-	lockRatio: 'Größenverhältnis beibehalten',
-	menu: 'Bild-Eigenschaften',
-	pathName: 'Bild',
-	pathNameCaption: 'Überschrift',
-	resetSize: 'Größe zurücksetzen',
-	resizer: 'Zum vergrößern anwählen und ziehen',
-	title: 'Bild-Eigenschaften',
-	uploadTab: 'Hochladen',
-	urlMissing: 'Imagequelle URL fehlt.',
 	sizes: 'Größenabgrenzung (responsive)',
 	srcset: 'Quellenverzeichnis (responsive)',
 } );
