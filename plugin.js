f74fdfb844778f59b383030f38538a26a70e006e
Clemens Krack <ckrack@gmail.com>
2014-12-18 14:33:36 +0100

    adding dialog fields at the correct contextual place. srcset belongs after src, sizes belongs after the width/height.

--- plugin.js
+++ plugin.js
@@ -30,7 +30,6 @@
         init: function(editor) {
             // bind to widget#instanceCreated so we can see when the image widget is about to be initiated
             editor.widgets.on('instanceCreated', function(e) {
-
                 var widget = e.data;
 
                 // figure out if this is the image dialog.
@@ -66,7 +65,7 @@
                 widget.setData(data);
             });
 
-            CKEDITOR.on( 'dialogDefinition', function(e) {
+            CKEDITOR.on('dialogDefinition', function(e) {
                 // make sure this is the right editor (there can be more on one page) and the right dialog.
                 if ((e.editor != editor) || (e.data.name != 'image2'))
                     return;
@@ -86,7 +85,7 @@
                     commit: function (widget) {
                         widget.setData('srcset', this.getValue());
                     }
-                    });
+                }, 'alt');
 
                 infoTab.add({
                     id: 'sizes',
@@ -99,7 +98,7 @@
                     commit: function (widget) {
                         widget.setData('sizes', this.getValue());
                     }
-                });
+                }, 'alignment');
             });
         }
     });
